f4a0526eec451ea5638481259fa6b6ddd5a39ed5
Livibleu <didier.desplanque@gmail.com>
2015-02-07 21:26:36 +0100

    Troisième mise à jour

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,10 +1,19 @@
+# Install libraries
 library("datasets")
 library("data.table")
+# Upload of data in initial variable
 household_power_consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
+# View the result
 View(household_power_consumption)
+# Transform a data.frame to a data.table
 DT=data.table(household_power_consumption)
+# Define what I am looking for (subset)
 select<-c("1/2/2007","2/2/2007")
+# Store the result in a variable
 data<-DT[DT$Date %in% select,]
+# Create histogram
 hist(data$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+# Copy the device screen to a device png
 dev.copy(png, file="plot1.png")
+# Close the connection
 dev.off()
--- figure/plot2.R
+++ figure/plot2.R
@@ -1,12 +1,22 @@
+# Install libraries
 library("datasets")
 library("data.table")
 library("lubridate")
+# Upload data in initial variable
 household_power_consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
+# View the result
 View(household_power_consumption,)
+# Transform a data.frame to a data.table
 DT=data.table(household_power_consumption)
+# Define what I am looking for (subset)
 select<-c("1/2/2007","2/2/2007")
+# Store the result in a variable
 data2<-DT[DT$Date %in% select,]
+# Add a new col to my data.table
 data2[,jour:=as.POSIXct(paste(as.Date(data2$Date, format="%d/%m/%Y"), data2$Time, sep=" "))]
+# Create plot
 plot(data2$jour,data2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+# Copy the device screen to a device png
 dev.copy(png, file="plot2.png")
+# Close the connection
 dev.off()
